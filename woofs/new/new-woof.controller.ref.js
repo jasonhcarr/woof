ece9e3a20d5c3140446ecfe625a3ec1c0c1157e1
Rob Andrews <andrews.robert.m@gmail.com>
2016-10-25 06:10:04 -0400

    Adds localStorage to woofs service

--- woofs/new/new-woof.controller.ref.js
+++
@@ -1,8 +0,0 @@
-angular.module('woof').controller('newWoofCtrl', function ($scope, Woofs, $state) {
-  $scope.maxTextLength = 120;
-
-  $scope.create = function () {
-    Woofs.create($scope.woofText);
-    $state.go('woof.index');
-  };
-});
--- woofs/woofs.service.js
+++ woofs/woofs.service.js
@@ -1,19 +1,47 @@
-angular.module('woof').service('Woofs', function (Woof) {
-  var woofs = [
-    new Woof({
-      body: 'OMG',
-      starred: true,
-      starCount: 5
-    }),
-    new Woof({
-      body: 'My breakfast was \u{1F4AF}',
-      starred: true,
-      starCount: 1
-    }),
-    new Woof({
-      body: 'I just got a soda.',
-      starred: false,
-      starCount: 0
+angular.module('woof').service('Woofs', function ($q, Woof, localStorageService) {
+  function getWoofs () {
+    return localStorageService.get('woofs') || [];
+  }
+
+  function setWoofs (woofs) {
+    localStorageService.set('woofs', woofs);
+  }
+
+  function findWoofById (woofs, id) {
+    var toReturn;
+    woofs.forEach(function (woof) {
+      if (woof.id === id) {
+        toReturn = woof;
+      }
     })
-  ];
+    return toReturn;
+  }
+
+  this.toggleStar = function (woof) {
+    var isPreviouslyStarred = woof.starred;
+    woof.starred = !isPreviouslyStarred;
+
+    if (isPreviouslyStarred) {
+      woof.starCount = woof.starCount - 1;
+    } else {
+      woof.starCount = woof.starCount + 1;
+    }
+
+    var woofs = getWoofs();
+    var woofToUpdate = findWoofById(woofs, woof.id);
+    angular.copy(woof, woofToUpdate);
+    setWoofs(woofs);
+  };
+
+  this.fetch = function () {
+    return $q.when(getWoofs());
+  };
+
+  this.create = function (woofText) {
+    var woofs = getWoofs();
+    woofs.push(new Woof({
+      body: woofText
+    }));
+    return setWoofs(woofs);
+  };
 });
